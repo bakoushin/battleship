22b1fa09e1b4da4b244e08a45ed9877da8257d35
Alex Bakoushin <alex@bakoush.in>
2025-05-10 17:51:33 +0200

    fix skeleton

--- frontend/src/components/BoardSkeleton/index.tsx
+++ frontend/src/components/BoardSkeleton/index.tsx
@@ -6,7 +6,7 @@ export default function BoardSkeleton() {
   return (
     <div className={classes['board-skeleton']}>
       {Array.from({ length: BOARD_SIZE * BOARD_SIZE }).map((_, idx) => (
-        <div className={classes['skeleton-cell']} key={idx} />
+        <div className={`${classes['skeleton-cell']} ${classes['cell']}`} key={idx} />
       ))}
     </div>
   )
